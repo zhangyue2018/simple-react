0e3a924104f8ab5d38c8d811fa9927815727dbfd
ZY <1822545638@qq.com>
2023-08-30 03:07:08 +0800

    getSnapshotBeforeUpdate案例

--- src/index.js
+++ src/index.js
@@ -1,57 +1,74 @@
 // import React from 'react';
-import React from './react';
-import ReactDOM from './react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class DerivedState extends React.Component {
+class ScrollingList extends React.Component {
+    isAppend = true;
+    count = 0;
+    intervalId = 0;
+
     constructor(props) {
         super(props);
-        this.state = {
-            preveUserId: 'zhangsanfeng',
-            email: 'zhangsanfeng@xxx.com'
-        }
+        this.listRef = React.createRef();
+        this.state = { list: [] }
     }
 
-    static getDerivedStateFromProps(props, state) {
-        if(props.userId !== state.preveUserId) {
-            return {
-                preveUserId: props.userId,
-                email: props.userId + '@xxx.com'
-            }
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        if(prevState.list.length < this.state.list.length) {
+            const list = this.listRef.current;
+            return list.scrollHeight - list.scrollTop;
         }
         return null;
     }
 
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(snapshot !== null) {
+            const list = this.listRef.current;
+            list.scrollTop = list.scrollHeight - snapshot;
+        }
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+    }
+
+    appendData() {
+        if(this.isAppend) {
+            this.intervalId = setInterval(() => {
+                this.setState({
+                    list: [...this.state.list, this.count++]
+                });
+            }, 1000);
+        } else {
+            clearInterval(this.intervalId);
+        }
+
+        this.isAppend = !this.isAppend;
+    }
+
     render() {
         return <div>
-            <h1>Email:</h1>
-            <h2>{ this.state.email }</h2>
+            <input type='button' onClick={() => this.appendData()} value="追加/暂停追加"/>
+            <div ref={this.listRef} style={{
+                overflow: 'auto',
+                height: '400px',
+                background: '#efefef'
+            }}>{
+                this.state.list.map(item => {
+                    return <div key={item} style={{
+                        height: '60px',
+                        padding: '10px',
+                        marginTop: '10px',
+                        border: '1px solid blue',
+                        borderRadius: '6px'
+                    }}>{item}</div>
+                })
+            }</div>
         </div>
     }
 }
 
-class ParentClass extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {id: 'zhangsanfeng'}
-    }
-
-    changeUserId = () => {
-        this.setState({
-            id: 'dongfangbubai'
-        });
-    }
-
-    render() {
-        console.log('render');
-        return <div>
-            <input type="button" value="点击改变UserId" onClick={() => this.changeUserId()} />
-            <DerivedState userId={this.state.id} />
-        </div>
-    }
-
-}
 
 
-
-ReactDOM.render(<ParentClass />, document.getElementById('root'));
+ReactDOM.render(<ScrollingList />, document.getElementById('root'));
 
