dc259458ab5f465f20371e5d6df3c918f4ea8c6f
ZY <1822545638@qq.com>
2023-08-30 18:04:03 +0800

    pureComponent案例

--- src/index.js
+++ src/index.js
@@ -1,74 +1,51 @@
 // import React from 'react';
-import React from './react';
-import ReactDOM from './react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class ScrollingList extends React.Component {
-    isAppend = true;
-    count = 0;
-    intervalId = 0;
+class Greeting extends React.PureComponent {
+    render() {
+        console.log('Greeting render');
+        return <div>
+            <h3>Hello{this.props.name && ','}{this.props.name}</h3>
+        </div>
+    }
+}
 
+class MyApp extends React.Component {
     constructor(props) {
         super(props);
-        this.listRef = React.createRef();
-        this.state = { list: [] }
+        this.state = { name: '', address: '' }
     }
 
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        if(prevState.list.length < this.state.list.length) {
-            const list = this.listRef.current;
-            return list.scrollHeight - list.scrollTop;
-        }
-        return null;
+    setName = (newName) => {
+        this.setState({
+            name: newName
+        });
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if(snapshot !== null) {
-            const list = this.listRef.current;
-            list.scrollTop = list.scrollHeight - snapshot;
-        }
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.intervalId);
-    }
-
-    appendData() {
-        if(this.isAppend) {
-            this.intervalId = setInterval(() => {
-                this.setState({
-                    list: [...this.state.list, this.count++]
-                });
-            }, 1000);
-        } else {
-            clearInterval(this.intervalId);
-        }
-
-        this.isAppend = !this.isAppend;
+    setAddress = (newAddress) => {
+        this.setState({
+            address: newAddress
+        });
     }
 
     render() {
+        console.log('MyApp');
         return <div>
-            <input type='button' onClick={() => this.appendData()} value="追加/暂停追加"/>
-            <div ref={this.listRef} style={{
-                overflow: 'auto',
-                height: '400px',
-                background: '#efefef'
-            }}>{
-                this.state.list.map(item => {
-                    return <div key={item} style={{
-                        height: '60px',
-                        padding: '10px',
-                        marginTop: '10px',
-                        border: '1px solid blue',
-                        borderRadius: '6px'
-                    }}>{item}</div>
-                })
-            }</div>
+            <label>
+                Name:
+                <input onInput={e => this.setName(e.target.value)} />
+            </label>
+            <label>
+                Address:
+                <input onInput={e => this.setAddress(e.target.value)} />
+            </label>
+            <Greeting name={this.state.name} />
         </div>
     }
 }
 
 
 
-ReactDOM.render(<ScrollingList />, document.getElementById('root'));
+ReactDOM.render(<MyApp />, document.getElementById('root'));
 
