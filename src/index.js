b9f5883aa5e22afed65b55eed41db084dffd71ec
ZY <1822545638@qq.com>
2023-08-31 18:27:49 +0800

    useState案例观察

--- src/index.js
+++ src/index.js
@@ -1,56 +1,18 @@
-// import React from 'react';
-import React from './react';
-import ReactDOM from './react-dom';
+import React from 'react';
+import { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-// class Greeting extends React.PureComponent {
-//     render() {
-//         console.log('Greeting render');
-//         return <div>
-//             <h3>Hello{this.props.name && ','}{this.props.name}</h3>
-//         </div>
-//     }
-// }
-
-const Greeting = React.memo(function Greeting({ name }) {
-    console.log('Greeting was rendered at', new Date().toLocaleTimeString());
-    return <h3>Hello{name && ','}{name}</h3>
-});
-
-class MyApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { name: '', address: '' }
+function Counter() {
+    const [count, setCount] = useState(0);
+    function handleClick() {
+        setCount(count + 1);
     }
-
-    setName = (newName) => {
-        this.setState({
-            name: newName
-        });
-    }
-
-    setAddress = (newAddress) => {
-        this.setState({
-            address: newAddress
-        });
-    }
-
-    render() {
-        console.log('MyApp');
-        return <div>
-            <label>
-                Name:
-                <input onInput={e => this.setName(e.target.value)} />
-            </label>
-            <label>
-                Address:
-                <input onInput={e => this.setAddress(e.target.value)} />
-            </label>
-            <Greeting name={this.state.name} />
-        </div>
-    }
+    return <button onClick={handleClick}>
+        You pressed me {count} times.
+    </button>
 }
 
 
 
-ReactDOM.render(<MyApp />, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById('root'));
 
---
+++ src/index_4.js
@@ -0,0 +1,56 @@
+// import React from 'react';
+import React from './react';
+import ReactDOM from './react-dom';
+
+// class Greeting extends React.PureComponent {
+//     render() {
+//         console.log('Greeting render');
+//         return <div>
+//             <h3>Hello{this.props.name && ','}{this.props.name}</h3>
+//         </div>
+//     }
+// }
+
+const Greeting = React.memo(function Greeting({ name }) {
+    console.log('Greeting was rendered at', new Date().toLocaleTimeString());
+    return <h3>Hello{name && ','}{name}</h3>
+});
+
+class MyApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: '', address: '' }
+    }
+
+    setName = (newName) => {
+        this.setState({
+            name: newName
+        });
+    }
+
+    setAddress = (newAddress) => {
+        this.setState({
+            address: newAddress
+        });
+    }
+
+    render() {
+        console.log('MyApp');
+        return <div>
+            <label>
+                Name:
+                <input onInput={e => this.setName(e.target.value)} />
+            </label>
+            <label>
+                Address:
+                <input onInput={e => this.setAddress(e.target.value)} />
+            </label>
+            <Greeting name={this.state.name} />
+        </div>
+    }
+}
+
+
+
+ReactDOM.render(<MyApp />, document.getElementById('root'));
+
