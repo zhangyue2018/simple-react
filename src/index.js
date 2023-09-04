79fcb58ca38f03c0f34f41e5c3c80a8a4d5fc32d
ZY <1822545638@qq.com>
2023-09-05 01:44:25 +0800

    useRef案例和源码实现

--- src/index.js
+++ src/index.js
@@ -1,60 +1,18 @@
 // import React from 'react';
-import React, { useEffect, useState } from './react';
+import React, { useRef } from './react';
 import ReactDOM from './react-dom';
 
-export function createConnection(serverUrl, roomId) {
-    return {
-        connect() {
-            console.log('√ Connecting to "' + roomId + '" room at ' + serverUrl + '...');
-        },
-        disconnect() {
-            console.log('× Disconnected from "' + roomId + '" room at ' + serverUrl);
-        }
-    };
-}
-
-function ChatRoom({ roomId }) {
-    const [ serverUrl, setServerUrl ] = useState('http://localhost:1234');
-    useEffect(() => {
-        const connection = createConnection(serverUrl, roomId);
-        connection.connect();
-        return () => {
-            connection.disconnect();
-        };
-    }, [roomId, serverUrl]);
-
+function Form() {
+    const inputRef = useRef(null);
+    function handleClick() {
+        inputRef.current.focus();
+    }
     return <div>
-        <label>
-            Server URL: {' '}
-            <input value={serverUrl} onInput={e => setServerUrl(e.target.value)} />
-        </label>
-        <h1>Welcome to the {roomId} room!</h1>
+        <input ref={inputRef} />
+        <button onClick={handleClick}>Focus the input</button>
     </div>
 }
 
-export default function App() {
-    const [ roomId, setRoomId ] = useState('general');
-    const [ show, setShow ] = useState(false);
-    return (
-        <div>
-            <label>
-                Choose the chat room: {' '}
-                <select value={roomId} onChange={e => setRoomId(e.target.value)}>
-                    <option value='general'>general</option>
-                    <option value='travel'>travel</option>
-                    <option value='music'>music</option>
-                </select>
-            </label>
-            <button onClick={() => setShow(!show)}>
-                {show ? "Close chat" : "Open chat"}
-            </button>
-            {show && <hr/>}
-            {show && <ChatRoom roomId={roomId} />}
-        </div>
-    );
-}
 
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Form />, document.getElementById('root'));
 
---
+++ src/indexjs/index_7.js
@@ -0,0 +1,60 @@
+// import React from 'react';
+import React, { useEffect, useState } from '../react';
+import ReactDOM from '../react-dom';
+
+export function createConnection(serverUrl, roomId) {
+    return {
+        connect() {
+            console.log('√ Connecting to "' + roomId + '" room at ' + serverUrl + '...');
+        },
+        disconnect() {
+            console.log('× Disconnected from "' + roomId + '" room at ' + serverUrl);
+        }
+    };
+}
+
+function ChatRoom({ roomId }) {
+    const [ serverUrl, setServerUrl ] = useState('http://localhost:1234');
+    useEffect(() => {
+        const connection = createConnection(serverUrl, roomId);
+        connection.connect();
+        return () => {
+            connection.disconnect();
+        };
+    }, [roomId, serverUrl]);
+
+    return <div>
+        <label>
+            Server URL: {' '}
+            <input value={serverUrl} onInput={e => setServerUrl(e.target.value)} />
+        </label>
+        <h1>Welcome to the {roomId} room!</h1>
+    </div>
+}
+
+export default function App() {
+    const [ roomId, setRoomId ] = useState('general');
+    const [ show, setShow ] = useState(false);
+    return (
+        <div>
+            <label>
+                Choose the chat room: {' '}
+                <select value={roomId} onChange={e => setRoomId(e.target.value)}>
+                    <option value='general'>general</option>
+                    <option value='travel'>travel</option>
+                    <option value='music'>music</option>
+                </select>
+            </label>
+            <button onClick={() => setShow(!show)}>
+                {show ? "Close chat" : "Open chat"}
+            </button>
+            {show && <hr/>}
+            {show && <ChatRoom roomId={roomId} />}
+        </div>
+    );
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
