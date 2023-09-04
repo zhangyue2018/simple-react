cfd6b3b3dd355363c1bd9f948275717247c6a322
ZY <1822545638@qq.com>
2023-09-04 17:16:10 +0800

    修改历史文件的引入路径

--- src/indexjs/index_1.js
+++ src/indexjs/index_1.js
@@ -1,6 +1,6 @@
 // import React from 'react';
-import React from './react';
-import ReactDOM from './react-dom';
+import React from '../react';
+import ReactDOM from '../react-dom';
 
 class Clock extends React.Component {
     constructor(props) {
--- src/indexjs/index_3.js
+++ src/indexjs/index_3.js
@@ -1,6 +1,6 @@
 // import React from 'react';
-import React from './react';
-import ReactDOM from './react-dom';
+import React from '../react';
+import ReactDOM from '../react-dom';
 
 class Greeting extends React.PureComponent {
     render() {
