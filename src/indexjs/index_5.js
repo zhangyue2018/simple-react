c444b078eb7ff5194f3f898a0ba1e350f5cee5d9
ZY <1822545638@qq.com>
2023-09-04 17:06:12 +0800

    文件整理

---
+++ src/indexjs/index_5.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+import React, { useState } from './react';
+import ReactDOM from './react-dom';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    function handleClick() {
+        setCount(count + 1);
+    }
+    return <button onClick={handleClick}>
+        You pressed me {count} times.
+    </button>
+}
+
+
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
+
